ccfa3f83d4d81454d096ea5beea0bc3c1537a0d5
DucTran <ductran2499@gmail.com>
2021-11-17 16:50:53 +0700

    update change password

--- src/public/assets/js/account.js
+++ src/public/assets/js/account.js
@@ -3,14 +3,25 @@ var viewInfo = document.getElementById("acc-with-info");
 var btnUpdate = document.getElementById("show-update");
 var viewUpdate = document.getElementById("update-info")
 var accMain = document.getElementById("acc-main");
+var btnPass = document.getElementById("btn-update-pass")
+var viewPass = document.getElementById("update-pass")
 btnInfo.onclick = function(){
     viewInfo.style.display = "block";
     viewUpdate.style.display = "none";
     accMain.style.display = "none";
+    viewPass.style.display = "none";
 }
 
 btnUpdate.onclick = function(){
     viewInfo.style.display = "none";
     viewUpdate.style.display = "block";
     accMain.style.display = "none";
+    viewPass.style.display = "none";
+}
+
+btnPass.onclick = function(){
+    viewInfo.style.display = "none";
+    viewUpdate.style.display = "none";
+    accMain.style.display = "none";
+    viewPass.style.display = "block";
 }
