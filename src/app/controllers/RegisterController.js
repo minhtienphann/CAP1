22e4e9da89becbb939f4630031778ed5a1d3397c
DucTran <ductran2499@gmail.com>
2021-11-26 14:47:15 +0700

    add user infor

--- src/app/controllers/RegisterController.js
+++ src/app/controllers/RegisterController.js
@@ -10,40 +10,43 @@ class RegisterController {
         res.render('register');
     }   
 
-    registing(req,res) {
-        let username = req.body.username;
-        let password1 = req.body.password1;
-        let password2 = req.body.password2;
-        if(password1==password2)
-        {
-            sql.connect(config, function(err) {
-                if(err)
-                {
-                    console.log('LOI KET NOI DB TU REGISTER'+err);
-                }
-                else
-                {
-                    var request = new sql.Request();
-                    request.query(request.template`insert into ACCOUNTUSER (acc_name,password)
-                     values(${username},${password2})`, function(error) {
-                        if(error)
-                        {
-                            console.log('register loi: '+error);
-                        }
-                        else
-                        {
-                            res.redirect('/login');
-                        }
-                    })
-                }
-            })
-        }
-        else
-        {
-            res.send('Mat khau 2 chua chinh xac');
-        }
-        
+    show(req,res,next) {
+        res.render('userinfor', {layout: false});
     }
+    // registing(req,res) {
+    //     let username = req.body.username;
+    //     let password1 = req.body.password1;
+    //     let password2 = req.body.password2;
+    //     if(password1==password2)
+    //     {
+    //         sql.connect(config, function(err) {
+    //             if(err)
+    //             {
+    //                 console.log('LOI KET NOI DB TU REGISTER'+err);
+    //             }
+    //             else
+    //             {
+    //                 var request = new sql.Request();
+    //                 request.query(request.template`insert into ACCOUNTUSER (acc_name,password)
+    //                  values(${username},${password2})`, function(error) {
+    //                     if(error)
+    //                     {
+    //                         console.log('register loi: '+error);
+    //                     }
+    //                     else
+    //                     {
+    //                         res.redirect('/login');
+    //                     }
+    //                 })
+    //             }
+    //         })
+    //     }
+    //     else
+    //     {
+    //         res.send('Mat khau 2 chua chinh xac');
+    //     }
+        
+    // }
     
 
 
--- src/routes/register.js
+++ src/routes/register.js
@@ -3,7 +3,12 @@ const router = express.Router();
 const registerController = require('../app/controllers/RegisterController');
 
 router.get('/',registerController.index);
-router.post('/registing',registerController.registing);
+
+router.get('/show',registerController.show);
+
+
+// router.post('/registing',registerController.registing);
+
 
 
 module.exports = router;
