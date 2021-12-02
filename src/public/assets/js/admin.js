ca3423dada02fca05387130faf6ca481d4ccfc6a
DucTran <ductran2499@gmail.com>
2021-12-02 18:23:05 +0700

    add statistical wed, add update user

--- src/public/assets/js/admin.js
+++ src/public/assets/js/admin.js
@@ -3,14 +3,25 @@ var viewInfo = document.getElementById("admin-with-info");
 var showUser = document.getElementById("list-user");
 var viewUser = document.getElementById("list-all-user");
 var accMain = document.getElementById("acc-main")
+var updateProfile = document.getElementById("update-all-user")
+var showUpdateProfile = document.getElementById("update-profile-user")
 btnInfo.onclick = function(){
     viewInfo.style.display = "block";
     viewUser.style.display = "none";
     accMain.style.display = "none";
+    showUpdateProfile.style.display = "none"
 }
 
 showUser.onclick = function(){
     viewInfo.style.display = "none";
     viewUser.style.display = "block";
     accMain.style.display = "none";
+    showUpdateProfile.style.display = "none"
+}
+
+updateProfile.onclick = function(){
+    viewInfo.style.display = "none";
+    viewUser.style.display = "none";
+    accMain.style.display = "none";
+    showUpdateProfile.style.display = "block"
 }
