cf288058c113ab3ad2685fa5478e3c67f70679e5
minhtienphann <Phanminhtien1304@gmail.com>
2021-12-13 17:17:43 +0700

    final

--- src/app/controllers/HomeController.js
+++ src/app/controllers/HomeController.js
@@ -15,43 +15,52 @@ class HomeController {
    
 
     give(req,res) {
-        var description = "";
-        description = 'Khau Trang: '+ req.body.khautrang + '\nRau Củ: ' + req.body.raucu + '\nGạo: ' + req.body.gao +
-        '\nMì Gói: ' + req.body.migoi + '\nSữa: ' + req.body.sua + '\nTrứng' + req.body.trung + '\nXe tải 4 Tấn: ' + req.body.xe4tan + '\nXe Tải 2 Tấn: ' + req.body.xe2tan + '\nGhi Chú : ' + req.body.comment;
-        if(req.cookies.accID == null)
+        if(req.cookies.accID != null)
         {
-            res.redirect('/login');
+            var description = "";
+            description = 'Khau Trang: '+ req.body.khautrang + '\nRau Củ: ' + req.body.raucu + '\nGạo: ' + req.body.gao +
+            '\nMì Gói: ' + req.body.migoi + '\nSữa: ' + req.body.sua + '\nTrứng' + req.body.trung + '\nXe tải 4 Tấn: ' + req.body.xe4tan + '\nXe Tải 2 Tấn: ' + req.body.xe2tan + '\nGhi Chú : ' + req.body.comment;
+            if(req.cookies.accID == null)
+            {
+                res.redirect('/login');
+            }
+            else
+            {
+                var id = req.cookies.accID;
+                sql.connect(config, function(error) {
+                    if(!error)
+                    {
+                        let request = new sql.Request();
+                        request.query(`update USERR set description='${description}', status='Muon Cho' where acc_id='${id}'`,
+                        function(err,result) {
+                            if(!err)
+                            {
+                                console.log('Update Thanh Cong !!!' + result);
+                                res.redirect('/account');
+                            }
+                            else
+                            {
+                                console.log('UPDATE FALSE !!!' + err)
+                                res.redirect('/');
+                            }
+                        })
+                    }
+                    else
+                    {
+                        console.log('CONNECT FALSE IN GIVE !!!' + error);
+                    }
+                })
+            }
         }
         else
         {
-            var id = req.cookies.accID;
-            sql.connect(config, function(error) {
-                if(!error)
-                {
-                    let request = new sql.Request();
-                    request.query(`update USERR set description='${description}', status='Muon Cho' where acc_id='${id}'`,
-                    function(err,result) {
-                        if(!err)
-                        {
-                            console.log('Update Thanh Cong !!!' + result);
-                            res.redirect('/account');
-                        }
-                        else
-                        {
-                            console.log('UPDATE FALSE !!!' + err)
-                            res.redirect('/');
-                        }
-                    })
-                }
-                else
-                {
-                    console.log('CONNECT FALSE IN GIVE !!!' + error);
-                }
-            })
+            res.redirect('/login')
         }
     }
 
     take(req,res) {
+       if(req.cookies.accID != null)
+       {
         var description = "";
         description ='So Ho Can Nhan: '+ req.body.soho + 'Khau Trang: '+ req.body.khautrang + 'Rau Củ: ' + req.body.raucu + 'Gạo: ' + req.body.gao +
         'Mì Gói: ' + req.body.migoi + 'Sữa: ' + req.body.sua + 'Trứng' + req.body.trung + 'Ghi Chu: ' + req.body.comment;
@@ -86,6 +95,11 @@ class HomeController {
                 }
             })
         }
+       }
+       else
+       {
+           res.redirect('/login')
+       }
     }
 }
 
